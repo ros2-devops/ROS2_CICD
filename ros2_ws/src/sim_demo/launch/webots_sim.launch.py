0f2879bd0110f16df4a26fbcd00db398359b6a05
AbdulazizQMUL <ec24136@qmul.ac.uk>
2025-06-16 14:59:41 +0000

    Add demo2 obstacle world and updated launch file

--- ros2_ws/src/sim_demo/launch/webots_sim.launch.py
+++ ros2_ws/src/sim_demo/launch/webots_sim.launch.py
@@ -4,33 +4,32 @@ from launch.substitutions import LaunchConfiguration
 import os
 
 def generate_launch_description():
-    scenario = LaunchConfiguration('scenario')
     duration = LaunchConfiguration('duration')
     log_interval = LaunchConfiguration('log_interval')
+    scenario = LaunchConfiguration('scenario')
 
-    world_path = [os.path.join(
-        os.getenv('HOME'),
-        'ros2_ws', 'src', 'sim_demo', 'worlds'
-    ), '/', scenario, '.wbt']
+    world_path = os.path.join(
+        os.getcwd(), 'ros2_ws', 'src', 'sim_demo', 'worlds',
+        LaunchConfiguration('scenario').perform({}) + '.wbt'
+    )
 
     return LaunchDescription([
-        DeclareLaunchArgument('scenario', default_value='demo1'),
         DeclareLaunchArgument('duration', default_value='10'),
         DeclareLaunchArgument('log_interval', default_value='1.0'),
+        DeclareLaunchArgument('scenario', default_value='demo1'),
 
         ExecuteProcess(
             cmd=[
-                'webots', '--stdout', '--batch',
-                ''.join(world_path)
+                'webots', '--stdout', '--batch', world_path
             ],
             additional_env={
                 'SIM_DURATION': duration,
-                'LOG_INTERVAL': log_interval
+                'LOG_INTERVAL': log_interval,
+                'SCENARIO': scenario
             },
             shell=True
         ),
 
-        # Start metrics node
         ExecuteProcess(
             cmd=['ros2', 'run', 'ros2_observability', 'metrics_collector'],
             shell=True,
