20281981014bd11f4480e49ec5a6daadef0ebee0
AbdulazizQMUL <ec24136@qmul.ac.uk>
2025-07-03 13:38:58 +0000

    trigger

--- check_feature_dist.py
+++ check_feature_dist.py
@@ -3,3 +3,4 @@ df = pd.read_csv('data_store/ros_metrics_all.csv',
                  names=['Time','CPU','Mem','CPU_roll','CPU_slope','Mem_roll','Mem_slope'])
 sns.pairplot(df[['CPU','Mem','CPU_roll','CPU_slope','Mem_roll','Mem_slope']])
 plt.savefig(f"feature_dist.png")
+
